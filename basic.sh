670d06918d5910ee8bc126daebb52869ac0c8c23
Ryosuke Morino <morinor@wit.edu>
2014-06-26 22:35:26 -0400

    bug fix

--- basic.sh
+++ basic.sh
@@ -51,12 +51,9 @@ else
             /bin/rpm -ivh http://ftp.riken.jp/Linux/fedora/epel/6/${ARC}/epel-release-6-8.noarch.rpm
             /bin/sed -i.org -e "s/enabled.*=.*1/enabled=1/g" /etc/yum.repos.d/epel.repo
 
-            #add RDO Icehouse repo
-            #/usr/bin/yum install -y http://rdo.fedorapeople.org/rdo-release.rpm
-
             #install ntp
             /usr/bin/yum install -y ntp
-            /usr/sbin/ntpdate server 0.centos.pool.ntp.org
+            /usr/sbin/ntpdate 0.centos.pool.ntp.org
             /sbin/chkconfig ntpd on
             /sbin/service ntpd restart
 
@@ -64,14 +61,22 @@ else
             /bin/sed -i.org -e 's/PermitRootLogin no/PermitRootLogin yes/gi' /etc/ssh/sshd_config
             /sbin/service sshd reload
 
-            #mysqls install
+            #install mysql
             /usr/bin/yum install -y mysql mysql-server MySQL-python
             /sbin/chkconfig mysqld on
-            /sbin/server mysqld restart
+            /sbin/service mysqld restart
 
+            #install QPID message server
+            /usr/bin/yum install qpid-cpp-server
+            /sbin/chkconfig qpidd on
+            /sbin/service qpidd restart
+            #install RDO Icehouse
+            #/usr/bin/yum install yum-plugin-priorities
+            /usr/bin/yum install -y http://rdo.fedorapeople.org/rdo-release.rpm
+            /usr/bin/yum install openstack-utils
 
-            #update package
-            /usr/bin/yum -y update
+            #upgrade package
+            /usr/bin/yum -y upgrade
 
             echo -e "${COLOR_LIGHT_BLUE}Inital configuration is done.${COLOR_DEFAULT}"
             echo -e "${COLOR_RED}Please reboot the system to apply all of the configuration${COLOR_DEFAULT}"
